58dd2689bcb07013d85fa658f154f4677b86bcde
Niklas Kopp <kopp@samuelson.de>
2019-11-27 11:30:18 +0100

    Update

--- Demo.AspNetCore.PushNotifications/Program.cs
+++ Demo.AspNetCore.PushNotifications/Program.cs
@@ -12,6 +12,7 @@ namespace Demo.AspNetCore.PushNotifications
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseIISIntegration();
     }
 }
